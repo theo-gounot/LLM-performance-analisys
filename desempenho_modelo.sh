8910eefc1ee10b843bde9a93987549b32a492f1f
Théo Gounot <136845592+theo-gounot@users.noreply.github.com>
2024-08-26 14:51:34 -0300

    Update desempenho_modelo.sh

--- desempenho_modelo.sh
+++ desempenho_modelo.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Command to run your Llama model
 your_llama_command="$HOME/llama.cpp"
 
@@ -10,13 +9,17 @@ log_file="main.log"
 documents_folder="$HOME/Documents/automacao"
 
 # Output file path in Documents
-output_file="$documents_folder/last_five_lines.log"
+cd
+cd Documents/automacao
+chmod u+w ~/Documents/automacao/
+output_file="$documents_folder/last_five_lines1.txt"
 
+cd 
+cd llama.cpp
 # Run the Llama model and wait for it to finish
-# Run the Llama model and wait for it to finish
-$HOME/llama-cli -m your_model.gguf -p "You are a helpful assistant" -cnv
+./llama-cli -m meta-llama-3.1-8b-instruct-q4_0.gguf -p "You are a helpful assistant" -cnv
 
 # After the command finishes, extract the last 5 lines from the log file
-tail -n 5 "$log_file" > "$output_file"
+tail -n 5 "$log_file" >> "$output_file"
 
 echo "Last 5 lines copied to $output_file"
